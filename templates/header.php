5a5c20ee034706cac6101a9c0e6f098294c108a7
carl alberto <cralberto11@gmail.com>
2016-04-13 22:47:06 +0800

    Added menu styles

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,14 @@
 <header class="banner">
   <div class="container">
     <?php if ( get_header_image() ) { ?>
-      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-        <img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-      </a>
+      <div class="logo-container">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+          <img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+        </a>
+      </div>
     <?php } else { ?>
       <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <?php } ?>  
+    <?php } ?>
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
